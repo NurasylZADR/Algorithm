e530df1004a3b16cf8f5a09fb8604052f585b10a
Nurasyl <nurasyl.alisher2004@gmail.com>
2022-05-08 18:44:51 +0600

    Second commit

---
+++ HashImplementation.java
@@ -0,0 +1,11 @@
+import java.util.Hashtable;
+
+public class HashImplementation {
+    public static void main(String[] args) {
+    Hashtable table = new Hashtable();
+
+    table.put(1, "abc");
+
+        System.out.println(table);
+    }
+}
